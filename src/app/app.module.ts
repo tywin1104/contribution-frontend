da916c7721966fc9b8e4f2a5a494bdf12a328be6
Tywin <tiaven1104@gmail.com>
2018-09-25 23:06:31 -0400

    styles basically works for landing page but need refactoring, js not yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +37,7 @@ export class AppModule {
     const authMiddleware = new ApolloLink((operation, forward) => {
       // add the authorization to the headers
       operation.setContext({
-        headers: new HttpHeaders().set('Authorization', "Bearer 42e9310236d53cd01f96c6a2da4b60acec8160ff")
+        headers: new HttpHeaders().set('Authorization', "XXX")
       });
 
       return forward(operation);
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: [
+    '../../assets/css/animate.min.css',
+    '../../assets/css/bootstrap.min.css',
+    '../../assets/css/etline-font.css',
+    '../../assets/css/flexslider.css',
+    '../../assets/css/ie.css',
+    '../../assets/css/normalize.min.css',
+    '../../assets/css/print.css',
+    './landing-page.component.css',
+    '../../assets/css/styles.css'
+  ]
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
